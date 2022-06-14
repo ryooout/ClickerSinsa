f7e8d8b8324ce69a03086a4fabc2e89dc972841d
ryooout <ryota01010104@gmail.com>
2022-06-14 21:07:59 +0900

    パーティクル生成

--- Assets/2Dscripts/ParticleManager.cs
+++ Assets/2Dscripts/ParticleManager.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ParticleManager : MonoBehaviour
-{
-    // Start is called before the first frame update
+{ 
     void Start()
     {
         Invoke(nameof(DestroyParticle),0.5f);
     }
-
-    // Update is called once per frame
     void Update()
     {
         
