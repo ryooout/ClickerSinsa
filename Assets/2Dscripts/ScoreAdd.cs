531160ac9c49dcb0494f85a406f4509e20378bfd
ryooout <ryota01010104@gmail.com>
2022-06-18 10:42:11 +0900

    余計なコードの削除

--- Assets/2Dscripts/ScoreAdd.cs
+++ Assets/2Dscripts/ScoreAdd.cs
@@ -19,28 +19,24 @@ public class ScoreAdd : MonoBehaviour
             Instantiate(gameManager.particle, transform.position, transform.rotation);
             gameManager.AddScore(5);
             Debug.Log(gameManager.score);
-            gameObject.GetComponent<Renderer>().enabled = false;
         }
         else if (Input.GetMouseButtonUp(0) && gameObject.tag == "Ojisan1")
         {
             Instantiate(gameManager.particle, transform.position, transform.rotation);
             gameManager.AddScore(10);
             Debug.Log(gameManager.score);
-            gameObject.GetComponent<Renderer>().enabled = false;
         }
         else if (Input.GetMouseButtonUp(0) && gameObject.tag == "Ojisan2")
         {
             Instantiate(gameManager.particle, transform.position, transform.rotation);
             gameManager.AddScore(15);
             Debug.Log(gameManager.score);
-            gameObject.GetComponent<Renderer>().enabled = false;
         }
         else if (Input.GetMouseButtonUp(0) && gameObject.tag == "Ojisan3")
         {
             Instantiate(gameManager.particle, transform.position, transform.rotation);
             gameManager.AddScore(20);
             Debug.Log(gameManager.score);
-            gameObject.GetComponent<Renderer>().enabled = false;
         }
         else if(Input.GetMouseButtonUp(0) && gameObject.tag =="DefaultOjisan")
         {
