389e945990c2cd5dad035933728fd3514791eeb3
ryooout <ryota01010104@gmail.com>
2022-06-18 16:57:16 +0900

    SEの追加

--- Assets/2Dscripts/ScoreAdd.cs
+++ Assets/2Dscripts/ScoreAdd.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class ScoreAdd : MonoBehaviour
 {
     [SerializeField] GameManager gameManager = default;
+    [SerializeField] AudioManager audioManager = default;
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        audioManager = GameObject.Find("AudioObj").GetComponent<AudioManager>();
     }
     void Update()
     {
@@ -19,29 +21,34 @@ public class ScoreAdd : MonoBehaviour
             Instantiate(gameManager.particle, transform.position, transform.rotation);
             gameManager.AddScore(5);
             Debug.Log(gameManager.score);
+            audioManager.SE();
         }
         else if (gameObject.tag == "Ojisan1")
         {
             Instantiate(gameManager.particle, transform.position, transform.rotation);
             gameManager.AddScore(10);
             Debug.Log(gameManager.score);
+            audioManager.SE();
         }
         else if (gameObject.tag == "Ojisan2")
         {
             Instantiate(gameManager.particle, transform.position, transform.rotation);
             gameManager.AddScore(15);
             Debug.Log(gameManager.score);
+            audioManager.SE();
         }
         else if (gameObject.tag == "Ojisan3")
         {
             Instantiate(gameManager.particle, transform.position, transform.rotation);
             gameManager.AddScore(20);
             Debug.Log(gameManager.score);
+            audioManager.SE();
         }
         else if(gameObject.tag =="DefaultOjisan")
         {
             gameManager.AddScore(1);
             Debug.Log(gameManager.score);
+            audioManager.SE();
         }
     }
 }
