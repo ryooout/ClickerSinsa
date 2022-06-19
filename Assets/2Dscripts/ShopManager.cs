93bb7c4a854ba2e6d4e838a416e8f5089b31e3b2
ryooout <ryota01010104@gmail.com>
2022-06-19 09:46:12 +0900

    ショップアイテムのプレファブの生成

--- Assets/2Dscripts/ShopManager.cs
+++ Assets/2Dscripts/ShopManager.cs
@@ -19,6 +19,15 @@ public class ShopManager : MonoBehaviour
     public int supplement = 1000;
     /// <summary>お金</summary>
     public int money = 5000;
+    [SerializeField] GameObject[] itemPrefab = default;
+    float x0 = -12;
+    float x1 = -12;
+    float x2 = -12;
+    float x3 = -12;
+    float y0 = 2;
+    float y1 = 0;
+    float y2 = -2;
+    float y3 = -4;
     private void Awake()
     {
        AddObj.SetActive(false);
@@ -36,7 +45,10 @@ public class ShopManager : MonoBehaviour
             gameManager.score -= cane;
             cane += 13;
             autoAdd.number++;
-            
+            Instantiate(itemPrefab[0],new Vector2(x0,y0),Quaternion.identity);
+            if (x0 >= 12.0f)
+            { x0 = -12; }
+            x0 += 0.15f;
         });
         shopButton[1].onClick.AddListener(() =>
         {　　　　　　　　　　　　　　　　　　　
@@ -45,6 +57,10 @@ public class ShopManager : MonoBehaviour
             gameManager.score -= wheelchair;
             wheelchair += 133;
             autoAdd.number += 3;
+            Instantiate(itemPrefab[1], new Vector2(x1, y1), Quaternion.identity);
+            if (x1 >= 12.0f)
+            { x1 = -12; }
+            x1 += 0.3f;
         });
         shopButton[2].onClick.AddListener(() =>
         {                                        
@@ -54,6 +70,10 @@ public class ShopManager : MonoBehaviour
             supplement += 1163;
             autoAdd.number += 5;
             gameManager.delete += 0.2f;
+            Instantiate(itemPrefab[2], new Vector2(x2, y2), Quaternion.identity);
+            if (x2 >= 12.0f)
+            { x2 = -12; }
+            x2 += 0.3f;
         });
         shopButton[3].onClick.AddListener(() =>
         {
@@ -63,6 +83,11 @@ public class ShopManager : MonoBehaviour
             money += 2103;
             autoAdd.number += 15;
             gameManager.delete += 0.35f;
+            Instantiate(itemPrefab[3], new Vector2(x3, y3), Quaternion.identity);
+            gameManager.AddScore(350);
+            if(x3 >= 12.0f)
+            { x3 = -12; }
+            x3 += 0.3f;
         });
     }
     void Update()
