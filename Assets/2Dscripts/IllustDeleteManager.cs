cd2847b46181c5f99c7aeb73e4b2a977020c523e
ryooout <ryota01010104@gmail.com>
2022-06-20 16:03:22 +0900

    SEの追加

--- Assets/2Dscripts/IllustDeleteManager.cs
+++ Assets/2Dscripts/IllustDeleteManager.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class IllustDeleteManager : MonoBehaviour
 { 
     [SerializeField]ShopManager shopManager = default;
+    [SerializeField]AudioManager audioManager = default;
     void Start()
     {      
         shopManager = GameObject.Find("ShopManager").GetComponent<ShopManager>();
+        audioManager = GameObject.Find("AudioObj").GetComponent<AudioManager>();
     }
 
     // Update is called once per frame
@@ -19,21 +21,25 @@ public class IllustDeleteManager : MonoBehaviour
         if (gameObject.tag == "Tue")
         {
             shopManager.auto += 0.07f;
+            audioManager.SE();
             Debug.Log("ñ‘‚¦‚½");
         }
         else if (gameObject.tag == "Kurumaisu")
         {
             shopManager.auto1 += 0.11f;
+            audioManager.SE();
             Debug.Log("Ô‚¢‚·‘‚¦‚½");
         }
         else if (gameObject.tag == "Sapuri")
         {
             shopManager.auto2 += 0.13f;
+            audioManager.SE();
             Debug.Log("ƒTƒvƒŠ‘‚¦‚½");
         }
         else if (gameObject.tag == "Otoshidama")
         {
             shopManager.auto3 += 0.15f;
+            audioManager.SE();
             Debug.Log("‚¨”N‹Ê‘‚¦‚½");
         }
     }
