425ea1bb28eab91bcc9149ba3de3ea9d5020d5a4
ryooout <ryota01010104@gmail.com>
2022-06-20 22:54:02 +0900

    他クラスの呼び出し

--- Assets/2Dscripts/ScoreAdd.cs
+++ Assets/2Dscripts/ScoreAdd.cs
@@ -6,18 +6,15 @@ public class ScoreAdd : MonoBehaviour
 {
     [SerializeField] GameManager gameManager = default;
     [SerializeField] AudioManager audioManager = default;
+    [SerializeField] ShopManager shopManager = default;
     int add = 5;
     int add1 = 10;
     int add2 = 15;
     int add3 = 20;
     int add4 = 1;
-   public float increse = 1;
-   public float increse1 = 1;
-   public float increse2 = 1;
-   public float increse3 = 1;
-   public float increse4 = 1;
     void Start()
     {
+        shopManager = GameObject.Find("ShopManager").GetComponent<ShopManager>();
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         audioManager = GameObject.Find("AudioObj").GetComponent<AudioManager>();
     }
@@ -29,34 +26,34 @@ public class ScoreAdd : MonoBehaviour
         if (gameObject.tag == "Ojisan")
         {
             Instantiate(gameManager.particle, transform.position, transform.rotation);
-            gameManager.AddScore(Mathf.FloorToInt(add*increse));
+            gameManager.AddScore(Mathf.FloorToInt(add+shopManager.inc));
             Debug.Log(gameManager.score);
             audioManager.SE();
         }
         else if (gameObject.tag == "Ojisan1")
         {
             Instantiate(gameManager.particle, transform.position, transform.rotation);
-            gameManager.AddScore(Mathf.FloorToInt(add1*increse1));
+            gameManager.AddScore(Mathf.FloorToInt(add1+shopManager.inc1));
             Debug.Log(gameManager.score);
             audioManager.SE();
         }
         else if (gameObject.tag == "Ojisan2")
         {
             Instantiate(gameManager.particle, transform.position, transform.rotation);
-            gameManager.AddScore(Mathf.FloorToInt(add2*increse2));
+            gameManager.AddScore(Mathf.FloorToInt(add2+shopManager.inc2));
             Debug.Log(gameManager.score);
             audioManager.SE();
         }
         else if (gameObject.tag == "Ojisan3")
         {
             Instantiate(gameManager.particle, transform.position, transform.rotation);
-            gameManager.AddScore(Mathf.FloorToInt(add3*increse3));
+            gameManager.AddScore(Mathf.FloorToInt(add3+shopManager.inc3));
             Debug.Log(gameManager.score);
             audioManager.SE();
         }
         else if(gameObject.tag =="DefaultOjisan")
         {
-            gameManager.AddScore(Mathf.FloorToInt(add4*increse4));
+            gameManager.AddScore(Mathf.FloorToInt(add4*shopManager.inc4));
             Debug.Log(gameManager.score);
             audioManager.SE();
         }
