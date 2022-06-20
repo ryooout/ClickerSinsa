aaa05580e780d908b9b109c7eeed82183b95ed7e
ryooout <ryota01010104@gmail.com>
2022-06-20 22:53:29 +0900

    ハートを取ると...?

--- Assets/2Dscripts/IllustDeleteManager.cs
+++ Assets/2Dscripts/IllustDeleteManager.cs
@@ -6,41 +6,49 @@ public class IllustDeleteManager : MonoBehaviour
 { 
     [SerializeField]ShopManager shopManager = default;
     [SerializeField]AudioManager audioManager = default;
+    [SerializeField]GameManager gameManager = default;
+    float rand;
     void Start()
     {      
         shopManager = GameObject.Find("ShopManager").GetComponent<ShopManager>();
         audioManager = GameObject.Find("AudioObj").GetComponent<AudioManager>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        rand = Random.Range(1.5f, 4.5f);
     }
     public void ItemClick()
     {
         if (gameObject.tag == "Tue")
         {
-            shopManager.auto += 0.07f;
+            shopManager.auto *= 1.25f;
             audioManager.SE();
             Debug.Log("ñ‘‚¦‚½");
         }
         else if (gameObject.tag == "Kurumaisu")
         {
-            shopManager.auto1 += 0.11f;
+            shopManager.auto1 *= 1.25f;
             audioManager.SE();
             Debug.Log("Ô‚¢‚·‘‚¦‚½");
         }
         else if (gameObject.tag == "Sapuri")
         {
-            shopManager.auto2 += 0.13f;
+            shopManager.auto2 *= 1.25f;
             audioManager.SE();
             Debug.Log("ƒTƒvƒŠ‘‚¦‚½");
         }
         else if (gameObject.tag == "Otoshidama")
         {
-            shopManager.auto3 += 0.15f;
+            shopManager.auto3 *= 1.25f;
             audioManager.SE();
             Debug.Log("‚¨”N‹Ê‘‚¦‚½");
+        }
+        else if(gameObject.tag =="GoldHeart")
+        {
+            gameManager.AddScore(Mathf.FloorToInt(gameManager.score*rand));
         }
     }
 }
