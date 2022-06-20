9302e008244ad37cac9bbba2ce9a1f652d656fa2
ryooout <ryota01010104@gmail.com>
2022-06-20 17:35:57 +0900

    Update AutoAdd.cs

--- Assets/2Dscripts/AutoAdd.cs
+++ Assets/2Dscripts/AutoAdd.cs
@@ -7,7 +7,7 @@ public class AutoAdd : MonoBehaviour
     [SerializeField]GameManager gameManager = default;
     float time = 0;
     float span = 1.0f;
-   public int number = 0;
+    public int number = 0;
     public int gatyaNumber = 0;
     void Start()
     {
